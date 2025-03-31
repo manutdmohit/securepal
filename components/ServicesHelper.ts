b5601e23252909d172fbc08c69a3aef41ea15fc4
Mohit Saud <mohit@prabidhilabs.com>
2025-03-31 14:59:13 +0545

    Separate sub-services

---
+++ components/ServicesHelper.ts
@@ -0,0 +1,19 @@
+export const services = [
+  { name: 'Full Service Package', href: '/services/full-service' },
+  { name: 'Website Design', href: '/services/website-design' },
+  { name: 'Website Hosting', href: '/services/website-hosting' },
+  { name: 'Router Installation', href: '/services/router-installation' },
+  { name: '24/7 IT Support', href: '/services/it-support' },
+  { name: 'VoIP Setup', href: '/services/voip-setup' },
+
+  { name: 'Professional Email', href: '/services/email-services' },
+  { name: 'Computer Repair', href: '/services/computer-repair' },
+  { name: 'Data Backup', href: '/services/data-backup' },
+  { name: 'Cybersecurity', href: '/services/cybersecurity' },
+  { name: 'Cloud Solutions', href: '/services/cloud-solutions' },
+];
+
+export const extraServices = [
+  { name: 'Phone Repair', href: '/services/phone-repair' },
+  { name: 'CCTV Monitoring', href: '/services/cctv-monitoring' },
+];
