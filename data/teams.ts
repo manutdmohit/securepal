f25299437ec676026adfd54ccba91f087c4abb1b
manutdmohit <saudmohit@gmail.com>
2026-09-25 08:53:24 +0545

    Remove descriptions from team member entries

--- data/teams.ts
+++ data/teams.ts
@@ -2,14 +2,12 @@ export const teams = [
   {
     name: 'Bishal Shiwakoti',
     role: 'IT Manager',
-    description: 'Passionate about protecting businesses from cyber threats.',
     photo: '/images/bishal-shiwakoti.png',
   },
   {
     id: 2,
     name: 'Sandeep Saud',
     role: 'SOC Analyst',
-    description: 'Expert in network security & cloud computing.',
     photo: '/images/sandeep-saud.png',
   },
 
@@ -17,21 +15,18 @@ export const teams = [
     id: 3,
     name: 'Malcolm John Tamang',
     role: 'Data Analytics & Engineering Consultant',
-    description: 'Experienced in big data analytics and data engineering.',
     photo: '/images/malcolm-john-tamang.png',
   },
   {
     id: 4,
     name: 'Mohit Saud',
     role: 'Web Developer',
-    description: 'Skilled in creating responsive and user-friendly websites.',
     photo: '/images/mohit-saud.png',
   },
   {
     id: 5,
     name: 'Roshan Baniya',
     role: 'IT Support Analyst',
-    description: 'Experienced in IT support.',
     photo: '/images/roshan-baniya.png',
   },
 ];
